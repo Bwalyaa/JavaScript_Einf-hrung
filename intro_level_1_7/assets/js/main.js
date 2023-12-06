ad7d69d8d28fcbf832bb7d3a1724282484412847
Bwalyaa <bwalya@hotmail.de>
2023-12-06 15:55:55 +0100

    intro level 1_7 hinzugefügt

---
+++ intro_level_1_7/assets/js/main.js
@@ -0,0 +1,21 @@
+let helloworld  = "Hello World";
+console.log({helloworld});
+
+const pi = 3.14;
+console.log({pi});
+
+let burjdubai = 824 + "m";
+console.log({burjdubai});
+
+const fullName = "Jan Schmidt"
+console.log({fullName});
+
+let eifeltower = 324 + "m";
+console.log({eifeltower});
+
+const camelCase = "camelCase";
+console.log({camelCase});
+
+let kursstatus = "true";
+console.log({kursstatus});
+
