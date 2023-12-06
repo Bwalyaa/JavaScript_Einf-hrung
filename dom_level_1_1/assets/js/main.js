4553fe0ca8b9a8d7e6c61078f0bf85dacaa0383c
Bwalyaa <bwalya@hotmail.de>
2023-12-06 15:15:55 +0100

    variablen level 1_6/dom level 1_1 fertig

---
+++ dom_level_1_1/assets/js/main.js
@@ -0,0 +1,17 @@
+document.write("<h2>Hello World</h2>")
+
+document.write("<p> Have a nice day!</p>")
+
+//second task
+
+let myText = "Hello again"
+
+document.write(myText)
+
+// last task
+
+const vorname = "Bwalya";
+
+const nachname = "Chanda";
+
+document.write(` Hi, ich bin SuperCoder:in ${vorname} ${nachname}`)
---
+++ variablen_level1_6/assets/js/main.js
@@ -0,0 +1,15 @@
+let x = 34;
+
+x = 67;
+
+console.log(x);
+
+// second task
+
+const y = 34;
+
+y = 67;
+
+console.log(y);
+
+// Ich bekomme eine Fehlermeldung, weil "Const" nicht überschrieben werden kann.
