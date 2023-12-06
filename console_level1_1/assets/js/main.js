c94d65a33fcb648121b4f799f3f38916841ee97d
Bwalyaa <bwalya@hotmail.de>
2023-12-06 14:24:58 +0100

    console 1_1 fertig

--- console_level1_1/assets/js/main.js
+++ console_level1_1/assets/js/main.js
@@ -9,4 +9,21 @@ console.log(b);
 console.log("Hello World!");
 
 //Variable firstName
-// const firstName = 
+const firstName = "Anton";
+let age = 28;
+
+console.log(firstName);
+console.log(age);
+
+//variable job/married
+
+let job = "Trainer:in";
+let married = true
+
+console.log(job);
+console.log(married);
+
+//Vollständiger Satz
+
+console.log(`${firstName} ist ${age} Jahre alt, von Beruf ${job} und ist verheiratet: ${married}`);
+
